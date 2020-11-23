faf5948e47710ea84d6e9a332aeae541e87bda80
haru93 <syuesuta@yahoo.co.jp>
2020-11-23 12:07:20 +0900

    first commit

---
+++ c_rankup/01_input.php
@@ -0,0 +1,25 @@
+<?php
+	/**
+	 * step1
+	 */
+	echo trim(fgets(STDIN));
+
+	/**
+	 * step2
+	 */
+	$count = trim(fgets(STDIN));
+
+    for ($i = 0; $i < $count; $i++) {
+        echo "paiza\n";
+	}
+	
+	/**
+	 * final
+	 */
+	$inputCount = trim(fgets(STDIN));
+
+    for ($i = 0; $i < $inputCount; $i++) {
+        [$name, $age] = explode(" ", trim(fgets(STDIN)));
+        $age++;
+        echo "$name $age" . PHP_EOL;
+    }
