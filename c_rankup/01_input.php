c6eb5b499d13f3edf1a5e4be92ac68a6e2b327c6
haru93 <syuesuta@yahoo.co.jp>
2020-11-23 12:09:23 +0900

    インデント修正

--- c_rankup/01_input.php
+++ c_rankup/01_input.php
@@ -1,25 +1,25 @@
 <?php
-	/**
-	 * step1
-	 */
-	echo trim(fgets(STDIN));
+/**
+ * step1
+ */
+echo trim(fgets(STDIN));
 
-	/**
-	 * step2
-	 */
-	$count = trim(fgets(STDIN));
+/**
+ * step2
+ */
+$count = trim(fgets(STDIN));
 
-    for ($i = 0; $i < $count; $i++) {
-        echo "paiza\n";
-	}
-	
-	/**
-	 * final
-	 */
-	$inputCount = trim(fgets(STDIN));
+for ($i = 0; $i < $count; $i++) {
+	echo "paiza\n";
+}
 
-    for ($i = 0; $i < $inputCount; $i++) {
-        [$name, $age] = explode(" ", trim(fgets(STDIN)));
-        $age++;
-        echo "$name $age" . PHP_EOL;
-    }
+/**
+ * final
+ */
+$inputCount = trim(fgets(STDIN));
+
+for ($i = 0; $i < $inputCount; $i++) {
+	[$name, $age] = explode(" ", trim(fgets(STDIN)));
+	$age++;
+	echo "$name $age" . PHP_EOL;
+}
