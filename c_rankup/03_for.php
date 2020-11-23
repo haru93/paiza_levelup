2020786f54408f01aa1c68ab2475c87b0c4ddb8f
haru93 <syuesuta@yahoo.co.jp>
2020-11-23 21:24:38 +0900

    Cランクレベルアップ：解答追加

--- c_rankup/03_for.php
+++ c_rankup/03_for.php
@@ -1,5 +1,34 @@
 <?php
 /**
+ * step4
+ * 
+ * 【解説】
+ * ・strpos関数を使い文字が含まれているかを判定
+ * 　判定結果は文字の先頭からの相対位置を指すことから、0が入力されること（先頭位置の場合）もあるため演算子 !== を使うことで厳格化
+ */
+$inputStrCountNum = trim(fgets(STDIN));
+    
+$strings = [];
+for ($i = 0; $i < $inputStrCountNum; $i++) {
+	$strings[] = trim(fgets(STDIN));
+}
+
+$inputCharCountNum = trim(fgets(STDIN));
+for ($i = 0; $i < $inputCharCountNum; $i++) {
+	$characters[] = trim(fgets(STDIN));
+}
+
+foreach ($strings as $string) {
+	foreach ($characters as $character) {
+		if (strpos($character, $string) !== false) {
+			echo "YES" . PHP_EOL;
+		} else {
+			echo "NO" . PHP_EOL;
+		}
+	}
+}
+
+/**
  * final
  */
 [$inputCountNum, $countNum, $scoreNum] = explode(" ", trim(fgets(STDIN)));
